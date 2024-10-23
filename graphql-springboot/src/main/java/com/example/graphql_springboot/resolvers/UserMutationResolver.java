4a7fa0f717bb1cfcef61d80103520fcc7e15a794
Mahfuzur Rahman Emon <mahfuzur.rahman@dsinnovators.com>
2024-10-23 20:25:04 +0600

    Add @Argument annotations in addUser method for GraphQL Mutation request handling
    - Added @Argument annotations to the parameters of the addUser method to explicitly tell Spring that these arguments will be passed from the GraphQL Mutation request.
    - This ensures that the method correctly maps GraphQL input to the method parameters.

--- graphql-springboot/src/main/java/com/example/graphql_springboot/resolvers/UserMutationResolver.java
+++ graphql-springboot/src/main/java/com/example/graphql_springboot/resolvers/UserMutationResolver.java
@@ -3,6 +3,7 @@ package com.example.graphql_springboot.resolvers;
 import com.example.graphql_springboot.model.User;
 import com.example.graphql_springboot.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.stereotype.Controller;
 
@@ -13,7 +14,7 @@ public class UserMutationResolver {
     public UserRepository userRepository;
 
     @MutationMapping
-    public User addUser(String name, String email) {
+    public User addUser(@Argument  String name, @Argument String email) {
         User user = new User();
         user.setName(name);
         user.setEmail(email);
